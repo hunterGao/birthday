68634a9069a6dd402cb73e0b4204fc010b7d35be
hunterGao <917516178@qq.com>
2019-12-05 17:19:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,27 +3,26 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "送给我",
+        "心爱的小宝贝",
         "今天是你的生日",
         "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~"
+        "第一个生日",
+        "和你一起看的周杰伦的演唱会",
+        "记忆依然清晰",
+        "要好好吃饭",
+        "要把我家可爱猪猪喂饱",
+        "然后抱回家"
     ],
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "SUPRISE",
+        play: "MUSIC",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Most Delicious Cake Ever",
+        light_candle: "Light Candle",
+        wish_message: "HAPPY BIRTHDAY",
         story: "A MESSAGE FOR YOU",
     }
 };
